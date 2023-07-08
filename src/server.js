82db3091d5c31b18952870fe32b02178a77a3d55
codethi <docencia.thiago@gmail.com>
2023-07-08 10:19:16 -0300

    feat: open cors

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express, { json } from "express";
 import authRouter from "./routes/authRoutes.js";
 import transactionRouter from "./routes/transactionRoutes.js";
 import { connectDb } from "./config/database.js";
+import cors from "cors";
 
 const app = express();
 
 connectDb();
 app.use(json());
+app.use(cors());
 app.use(authRouter);
 app.use(transactionRouter);
 
